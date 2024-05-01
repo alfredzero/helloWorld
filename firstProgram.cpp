98cb2621d3fc3d9fbe366d0729d3750e591de374
alfredzero <amezcuaalfredo81@gmail.com>
2024-04-30 21:05:43 -0700

    cpp hello world

---
+++ firstProgram.cpp
@@ -0,0 +1,18 @@
+//
+//  firstProgram.cpp
+//  cppHello
+//
+//  Created by Alfredo Amezcua on 4/30/24.
+//
+
+#include "firstProgram.hpp"
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
---
+++ firstProgram.hpp
@@ -0,0 +1,13 @@
+//
+//  firstProgram.hpp
+//  cppHello
+//
+//  Created by Alfredo Amezcua on 4/30/24.
+//
+
+#ifndef firstProgram_hpp
+#define firstProgram_hpp
+
+#include <stdio.h>
+
+#endif /* firstProgram_hpp */
